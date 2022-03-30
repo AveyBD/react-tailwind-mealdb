9e282eb23bf363cf51c014874c5e2b340a431f9b
Debashish D. Dev <hello@debashish.dev>
2022-03-31 00:03:42 +0600

    Added Meal Category Route Path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Home from "./components/Home/Home";
+import MealCategory from "./components/MealCategory/MealCategory";
 import NotFound from "./components/NotFound/NotFound";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<><Hero></Hero><Home></Home></>}></Route>
+        <Route path="/category" element={<MealCategory></MealCategory>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       
