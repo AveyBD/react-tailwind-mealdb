66c906806a8a8af8dc3f2cc0a72dda2ca69748d3
Debashish D. Dev <hello@debashish.dev>
2022-03-30 10:59:21 +0600

    Added Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import logo from './logo.svg'
+import React from 'react';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
     </div>
   );
 }
