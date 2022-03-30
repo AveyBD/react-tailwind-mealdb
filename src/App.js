c6efc8801eacc75aef7611b5ffafe9958436dfd2
Debashish D. Dev <hello@debashish.dev>
2022-03-30 17:06:07 +0600

    Added Home Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
+import React from "react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-white pt-6">
+      <h4 className="text-center text-mono">Fantastic In Town</h4>
+      <h2 className="text-center text-orange-600 font-bold text-3xl">
+        Have a look ar our Food Category!
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
