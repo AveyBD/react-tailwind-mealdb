8fab8184ee893ba42c44b788a14502bccfc36b54
Debashish D. Dev <hello@debashish.dev>
2022-03-31 01:10:59 +0600

    Changed Header Home link to Link from Anchor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../logo.svg";
 
 const Header = () => {
@@ -51,13 +52,13 @@ const Header = () => {
           >
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
-                <a
-                  href="/"
+                <Link
+                  to="/"
                   className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
                 <a
