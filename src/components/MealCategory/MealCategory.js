d77049436b07dd2f542070f51b80a5203224a3f2
Debashish D. Dev <hello@debashish.dev>
2022-03-31 11:33:21 +0600

    Fixed API URL Link

--- src/components/MealCategory/MealCategory.js
+++ src/components/MealCategory/MealCategory.js
@@ -5,7 +5,7 @@ const MealCategory = () => {
   const [cat, setCat] = useState([]);
 
   useEffect(() => {
-    const url = `https:/www.themealdb.com/api/json/v1/1/categories.php`;
+    const url = `https://www.themealdb.com/api/json/v1/1/categories.php`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setCat(data.categories));
